82fd5a398fcc4f0c58a07449c66eb87dc6d3ed44
brantje <brantje@gmail.com>
2016-04-19 19:20:44 +0200

    Fix useragent

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/', function (req, res) {
             'ignore-ssl-errors': req.query.ignoresslerrors
         },
         streamType: 'png',
-        userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
+        userAgent: req.query.userAgent
     };
     var filename = 'screenshot_' + utils.md5(url + JSON.stringify(options)) + '.png';
     var filePath = 'screenshots/' + filename;
