4b0a8e716b6329e1988df0918b04a5a4a2fbaf3d
brantje <brantje@gmail.com>
2016-04-20 08:39:00 +0200

    Added config file

---
+++ config/config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.web ={};
+config.web.port = process.env.WEB_PORT || 8080;
+config.FileCleanerService = {};
+config.FileCleanerService.link_cache = process.env.LINK_CACHE || 24 * 60 * 60000; // 1 day
+config.FileCleanerService.cache = process.env.OUTPUT_CACHE || 60000; // 1 minute
+module.exports = config;
