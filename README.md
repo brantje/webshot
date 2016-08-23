eaf05d312c9c2cddd9a9bbd9dcbe908c1fcf6373
Sander <sbrand@exed.nl>
2016-08-23 16:30:58 +0200

    Added watermark support

--- README.md
+++ README.md
@@ -10,37 +10,30 @@ Replace url with the actual url you want to take a screenshot from.
 <table>
     <tr>
         <td>Parameter</td>
-        <td>Alias</td>
         <td>Description</td>
     </tr>
     <tr>
         <td>url*</td>
-        <td>-</td>
         <td>URL from the website to take the website from</td>
     </tr>
     <tr>
         <td>height</td>
-        <td>h</td>
         <td>Height of the screen resolution (Default: 1080)</td>
     </tr>
     <tr>
         <td>width</td>
-        <td>w</td>
         <td>Width of the screen resolution (Default: 1920)</td>
     </tr>
     <tr>
         <td>quality</td>
-        <td>q</td>
         <td>JPEG compression quality. A higher number will look better, but creates a larger file.</td>
     </tr>
     <tr>
         <td>userAgent</td>
-        <td>u</td>
         <td>The useragent used when fetching the website</td>
     </tr>
     <tr>
         <td>ignoresslerrors</td>
-        <td>-</td>
         <td>Ignore ssl errors (Eg: self signed certs)</td>
     </tr>
     <tr>
@@ -50,7 +43,6 @@ Replace url with the actual url you want to take a screenshot from.
     </tr>
     <tr>
         <td>sendlink</td>
-        <td>-</td>
         <td>
             Give a link to the generated image. Images requested this way are kept for 24 hours.   
             Example: `http://webshot.local/?url=http://google.com&sendlink=true`
@@ -58,7 +50,6 @@ Replace url with the actual url you want to take a screenshot from.
     </tr>
     <tr>
         <td>filetype</td>
-        <td>-</td>
         <td>Used file type. Default: `png`. Other option(s): 'pdf`</td>
     </tr>
 </table>
--- server.js
+++ server.js
@@ -1,229 +1,259 @@
-module.exports = function(cluster,workerProcess) {
-    var config = require('./config/config.js');
-    var path = require('path');
-    var webshot = require('webshot');
-    var express = require('express');
-    var validUrl = require('valid-url');
-    var url = require("url");
-    var FileCleanerService = require('./lib/fileCleanerService');
-    var utils = require('./lib/utils');
-    var fs = require('fs');
-    var dns = require('dns');
-    var bodyParser = require('body-parser');
-    var app = express();
-    var fcs = new FileCleanerService(config.FileCleanerService.cache); //1 Minute
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
+module.exports = function (cluster, workerProcess) {
+  var config = require('./config/config.js');
+  var path = require('path');
+  var webshot = require('webshot');
+  var express = require('express');
+  var validUrl = require('valid-url');
+  var url = require("url");
+  var FileCleanerService = require('./lib/fileCleanerService');
+  var utils = require('./lib/utils');
+  var fs = require('fs');
+  var dns = require('dns');
+  var bodyParser = require('body-parser');
+  var app = express();
+  var fcs = new FileCleanerService(config.FileCleanerService.cache); //1 Minute
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
-    app.use("/screenshots", express.static(__dirname + '/screenshots'));
-    app.use("/tmp", express.static(__dirname + '/tmp'));
+  app.use("/screenshots", express.static(__dirname + '/screenshots'));
+  app.use("/tmp", express.static(__dirname + '/tmp'));
 
-    app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
 
-    var isEmpty = function (obj) {
 
-        // null and undefined are "empty"
-        if (obj == null) return true;
+  var getInjectableCSS = function(){
+    return css_injectables['watermark'];
+  };
 
-        // Assume if it has a length property with a non-zero value
-        // that that property is correct.
-        if (obj.length > 0)    return false;
-        if (obj.length === 0)  return true;
 
-        // If it isn't an object at this point
-        // it is empty, but it can't be anything *but* empty
-        // Is it empty?  Depends on your application.
-        if (typeof obj !== "object") return true;
 
-        // Otherwise, does it have any properties of its own?
-        // Note that this doesn't handle
-        // toString and valueOf enumeration bugs in IE < 9
-        for (var key in obj) {
-            if (hasOwnProperty.call(obj, key)) return false;
-        }
+  var screenshot = function (req, res) {
+    var filetype = 'png';
 
-        return true;
+    var loadFinished = function () {
+        console.log(this.watermark);
+        var s = '<div class="container">'+ this.watermark +'</div>'; // HTML string
+        var div = document.createElement('div');
+        div.className = 'watermark_wrapper';
+        div.innerHTML = s;
+        document.body.appendChild(div);
     };
 
-    var screenshot = function (req, res) {
-        var filetype = 'png';
-        if (isEmpty(req.query.url)  && isEmpty(req.body) ) {
-            res.json({'error': 'Required param missing'});
-            return;
-        }
-        if (req.query.url && !validUrl.isUri(req.query.url)) {
-            res.json({'error': 'Invalid url'});
-            return;
-        }
+    var css_rules = getInjectableCSS(req);
 
-        if (req.query.h) {
-            req.query.height = req.query.h;
-        }
-
-        if (req.query.w) {
-            req.query.width = req.query.w;
-        }
-
-        if (req.query.q) {
-            req.query.quality = req.query.q;
-        }
-
-        if (req.query.filetype) {
-            var allowed_types = ['png','pdf'];
-            if(allowed_types.indexOf(req.query.filetype) == -1){
-                res.json({'error': 'Invalid file type'});
-            }
-
-            filetype = req.query.filetype;
-        }
-
-        if (!req.query.height) {
-            req.query.height = 1080
-        }
-
-        if (!req.query.width) {
-            req.query.width = 1920
-        }
-
-        if (!req.query.quality) {
-            req.query.quality = 75
-        }
-
-        if (!req.query.ignoresslerrors) {
-            req.query.ignoresslerrors = false
-        }
-
-        if (!req.query.userAgent) {
-            req.query.userAgent = 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0';
-        }
-
-        if (!req.query.nocache) {
-            req.query.nocache = false;
-        }
-
-        var _sendlink = false;
-        if (req.query.sendlink) {
-            if(req.query.sendlink == 'true' || req.query.sendlink == true) {
-                _sendlink = true;
-            }
-        }
-
-
-
-        var options = {
-            url: req.query.url,
-            screenSize: {
-                width: req.query.width,
-                height: req.query.height
-            },
-            phanthomConfig: {
-                'ignore-ssl-errors': req.query.ignoresslerrors
-            },
-            streamType: 'png',
-            userAgent: req.query.userAgent,
-            extension: filetype
-        };
-        var filename_no_ext = 'screenshot_' + utils.md5(url + JSON.stringify(options));
-        filename = filename_no_ext + '.' + filetype;
-        var filePath = 'screenshots/' + filename;
-        var our_host = (req.secure) ? 'https://' + req.headers.host : 'http://' + req.headers.host;
-        //If an url is giving, fetch the webpage
-        if (req.query.url) {
-            if (fs.existsSync(filePath) && req.query.nocache === false) {
-                console.log('Request for %s - Found in cache', req.query.url);
-                if (!_sendlink) {
-                    res.sendFile(filePath, {root: __dirname}, function (err) {
-                        console.log('Done sending  cached file');
-                    });
-                } else {
-                    res.json({'link': our_host + '/' + filePath})
-                }
-
-                return;
-            }
-
-
-            var hostname = url.parse(req.query.url).hostname;
-            dns.resolve4(hostname, function (err, addresses) {
-                if (err) {
-                    console.log(err);
-                    res.json({'error': 'Hostname not found'});
-                } else {
-                    console.log('Taking screenshot from: ' + req.query.url);
-                    webshot(req.query.url, filePath, options, function (err) {
-                        if (!err) {
-
-                            if (!_sendlink) {
-                                res.sendFile(filePath, {root: __dirname}, function (err) {
-                                    console.log('Done sending file');
-                                    //fs.unlink('screenshots/' + filename);
-                                    fcs.addFile(filePath);
-                                });
-                            } else {
-                                res.json({'link': our_host + '/' + filePath});
-                                fcs.addFile(config.FileCleanerService.link_cache); //Timeout when sending a link @TODO make this a config option
-                            }
-
-
-                        } else {
-                            res.json({'error': 'PhantomJS exited with return value 1'});
-                        }
-                    });
-                }
-
-            });
-        }
-
-        if (req.body.html_string) {
-            var tmp_file = filename_no_ext + '.html';
-            fs.writeFile('tmp/' + tmp_file, req.body.html_string, function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-                var tmp_url = our_host + '/tmp/' + tmp_file;
-                webshot(tmp_url, filePath, options, function (err) {
-                    if (!err) {
-
-                        if (!_sendlink) {
-                            res.sendFile(filePath, {root: __dirname}, function (err) {
-                                console.log('Done sending file');
-                                //fs.unlink('screenshots/' + filename);
-                                fcs.addFile(filePath);
-                            });
-                        } else {
-                            res.json({'link': our_host + '/' + filePath});
-                            fcs.addFile(config.FileCleanerService.link_cache); //Timeout when sending a link
-                        }
-
-                        fs.unlinkSync('tmp/' + tmp_file);
-                    } else {
-                        res.json({'error': 'PhantomJS exited with return value 1'});
-                    }
-                });
-            });
-        }
+    var options = {
+      url: '',
+      screenSize: {},
+      shotSize: {
+        width: 'window',
+        height: 'window'
+      },
+      phanthomConfig: {
+        'ignore-ssl-errors': false
+      },
+      //customCSS: css_injectables['watermark'],
+      customCSS: css_rules,
+      streamType: 'png',
+      userAgent: 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0',
+      extension: filetype,
+      quality: 75,
+      useCache: true,
+      cookies: [],
+      customHeaders: null,
+      onLoadFinished: {
+        fn: loadFinished,
+        context: {watermark: config.watermark.text}
+      }
     };
 
-    app.get('/', screenshot);
-    app.post('/', screenshot);
 
-
-    var dirs = ['screenshots', 'tmp'];
-    for (var i = 0; i < dirs.length; i++) {
-        if (!fs.existsSync(dirs[i])) {
-            fs.mkdirSync(dirs[i]);
-        }
-
+    if (utils.isEmpty(req.query.url) && utils.isEmpty(req.body)) {
+      res.json({'error': 'Required param missing'});
+      return;
+    } else {
+      if (req.query.url) {
+        options.url = req.query.url;
+      }
+    }
+    if (req.query.url && !validUrl.isUri(req.query.url)) {
+      res.json({'error': 'Invalid url'});
+      return;
     }
 
-    app.listen(config.web.port, function () {
-        console.log('WebShot listening on port %s!', config.web.port);
+    if (req.query.filetype) {
+      var allowed_types = ['png', 'pdf'];
+      if (allowed_types.indexOf(req.query.filetype) == -1) {
+        res.json({'error': 'Invalid file type'});
+      }
+
+      filetype = req.query.filetype;
+    }
+
+    if (req.query.height) {
+      options.screenSize.height = req.query.height;
+    }
+
+    if (!req.query.width) {
+      options.screenSize.width = req.query.width;
+    }
+
+    if (req.query.quality) {
+      options.quality = req.query.quality;
+    }
+
+    if (req.query.ignoresslerrors) {
+      options.phanthomConfig['ignore-ssl-errors'] = true
+    }
+
+    if (req.query.userAgent) {
+      options.userAgent = req.query.userAgent;
+    }
+
+    if (req.query.nocache) {
+      options.useCache = false;
+    }
+
+    var _sendlink = false;
+    if (req.query.sendlink) {
+      if (req.query.sendlink == 'true' || req.query.sendlink == true) {
+        _sendlink = true;
+      }
+    }
+
+
+    if (req.query.watermark) {
+      if (req.query.watermark == 'false' || req.query.watermark == false) {
+        options.onLoadFinished = null;
+      }
+    }
+
+
+    var filename_no_ext = 'screenshot_' + utils.md5(url + JSON.stringify(options));
+    var filename = filename_no_ext + '.' + filetype;
+    var filePath = 'screenshots/' + filename;
+    var our_host = (req.secure) ? 'https://' + req.headers.host : 'http://' + req.headers.host;
+    //If an url is giving, fetch the webpage
+    if (req.query.url) {
+      if (fs.existsSync(filePath) && options.useCache === true) {
+        console.log('Request for %s - Found in cache', req.query.url);
+        if (!_sendlink) {
+          res.sendFile(filePath, {root: __dirname}, function (err) {
+            console.log('Done sending  cached file');
+          });
+        } else {
+          res.json({'link': our_host + '/' + filePath})
+        }
+
+        return;
+      }
+
+
+      var hostname = url.parse(req.query.url).hostname;
+      dns.resolve4(hostname, function (err, addresses) {
+        if (err) {
+          console.log(err);
+          res.json({'error': 'Hostname not found'});
+        } else {
+          console.log('Taking screenshot from: ' + req.query.url);
+          webshot(req.query.url, filePath, options, function (err) {
+            if (!err) {
+
+              if (!_sendlink) {
+                res.sendFile(filePath, {root: __dirname}, function (err) {
+                  console.log('Done sending file');
+                  //fs.unlink('screenshots/' + filename);
+                  fcs.addFile(filePath);
+                });
+              } else {
+                res.json({'link': our_host + '/' + filePath});
+                fcs.addFile(config.FileCleanerService.link_cache); //Timeout when sending a link @TODO make this a config option
+              }
+
+
+            } else {
+              res.json({'error': 'PhantomJS exited with return value 1'});
+            }
+          });
+        }
+
+      });
+    }
+
+    if (req.body.html_string) {
+      options.shotSize = {
+        width: 'all',
+        height: 'all'
+      };
+
+      var tmp_file = filename_no_ext + '.html';
+      fs.writeFile('tmp/' + tmp_file, req.body.html_string, function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        var tmp_url = our_host + '/tmp/' + tmp_file;
+        webshot(tmp_url, filePath, options, function (err) {
+          if (!err) {
+
+            if (!_sendlink) {
+              res.sendFile(filePath, {root: __dirname}, function (err) {
+                console.log('Done sending file');
+                //fs.unlink('screenshots/' + filename);
+                fcs.addFile(filePath);
+              });
+            } else {
+              res.json({'link': our_host + '/' + filePath});
+              fcs.addFile(config.FileCleanerService.link_cache); //Timeout when sending a link
+            }
+
+            fs.unlinkSync('tmp/' + tmp_file);
+          } else {
+            res.json({'error': 'PhantomJS exited with return value 1'});
+          }
+        });
+      });
+    }
+  };
+
+
+  /**
+   * Gather all injectable css files
+   */
+
+  var css_injectables = [];
+  fs.readdir('css_inject', function(err, files) {
+    if (err) return;
+    files.forEach(function(f) {
+      fs.readFile('css_inject/'+ f, 'utf8', function (err,data) {
+        var filename = f.split('.')[0];
+        css_injectables[filename] = data
+      });
     });
+  });
+
+  /**
+   * End css injectables
+   */
+
+  app.get('/', screenshot);
+  app.post('/', screenshot);
+
+
+  var dirs = ['screenshots', 'tmp'];
+  for (var i = 0; i < dirs.length; i++) {
+    if (!fs.existsSync(dirs[i])) {
+      fs.mkdirSync(dirs[i]);
+    }
+
+  }
+
+  app.listen(config.web.port, function () {
+    console.log('WebShot listening on port %s!', config.web.port);
+  });
 
 };
 
-if(!module.parent){
-    require('./server')();
+if (!module.parent) {
+  require('./server')();
 }
